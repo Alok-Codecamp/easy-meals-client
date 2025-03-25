992599fb1297516f534dc9a263ac679700e1a3b6
Alok-Codecamp <alok61.bd@gmail.com>
2025-03-25 16:33:25 +0600

    banner done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import NavBar from '../shared/NavBar';
 import Image from 'next/image';
 import bannerDesktop from '@/assets/banner-desktop.png';
-import bannerMobile from '@/assets/banner-mobile.png'
+import bannerMobile from '@/assets/banner-mobile.png';
+import bannerTab from '@/assets/banner-tab.png';
 const Home = () => {
     return (
         <main>
@@ -12,7 +13,7 @@ const Home = () => {
                 {/* banner section  */}
                 <section className=''>
                     <Image src={bannerDesktop} alt='dexktop' className='hidden lg:block h-[450px]' />
-                    <Image src='/' alt=' tab' width={1000} height={1080} className='hidden md:block sm:hidden lg:hidden' />
+                    <Image src={bannerTab} alt=' tab' width={1000} height={1080} className='hidden md:block sm:hidden lg:hidden' />
                     <Image src={bannerMobile} width={600} height={700} alt='mobile' className='block md:hidden' />
 
                 </section>
