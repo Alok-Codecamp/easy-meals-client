3fb40c86957462ca741b18281df084d7791a47ed
Alok-Codecamp <alok61.bd@gmail.com>
2025-05-09 23:50:52 +0600

    search bar completed

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -7,15 +7,26 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
 
+type FieldValue = {
+    search: string
+}
 const SearchBar = () => {
+    const form = useForm<FieldValue>();
+
+    const onSubmit = (data: FieldValue) => {
+        console.log(data)
+    }
     return (
         <div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
                     <FormField
                         control={form.control}
-                        name="username"
+                        name="search"
                         render={({ field }) => (
                             <FormItem>
                                 <FormLabel>Username</FormLabel>
